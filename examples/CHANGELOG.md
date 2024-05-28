a0228b5a27f8a864cf14b367e8a0c49f0e0c1792
Nikita Tikhonov <nekitdevofficial@gmail.com>
2024-05-28 22:32:38 +0300

    Bump versions everywhere.

--- examples/CHANGELOG.md
+++ examples/CHANGELOG.md
@@ -2,7 +2,7 @@
 
 <!-- changelogging: start -->
 
-## [0.1.0](https://github.com/nekitdev/changelogging/tree/v0.1.0) (YYYY-MM-DD)
+## [0.2.0](https://github.com/nekitdev/changelogging/tree/v0.2.0) (YYYY-MM-DD)
 
 ### Features
 
--- src/lib.rs
+++ src/lib.rs
@@ -108,7 +108,7 @@
 //!
 //! ```console
 //! $ changelogging preview
-//! ## [0.1.0](https://github.com/nekitdev/changelogging/tree/v0.1.0) (YYYY-MM-DD)
+//! ## [0.2.0](https://github.com/nekitdev/changelogging/tree/v0.2.0) (YYYY-MM-DD)
 //!
 //! ### Features
 //!
